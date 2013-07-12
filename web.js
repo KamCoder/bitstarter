5d69671651cd48ac1a81d67ce300dc98fc2164ea
Kamal <kamal.komallapalli@gmail.com>
2013-07-12 22:10:02 +0000

    modified web

--- web.js
+++ web.js
@@ -3,17 +3,18 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-fs.readFile('/home/ubuntu/bitstarter/index.html', function (err, fileContents) {
+var out = fs.readFileSync('index.html', function (err, data) {
     if (err) {
         throw err; 
-    }       
+	console.log(data);
+    });       
 
-	app.get('/', function(request, response) {
-		response.send(fileContents);
-	});
+app.get('/', function(request, response) {
+	response.send(out);
+});
 	
-	var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 	app.listen(port, function() {
 		console.log("Listening on " + port);
-	});
+	
 });
