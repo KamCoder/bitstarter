d40a1f101d1487c48dbd13a5b1f073ccbd08c3a2
Kamal <kamal.komallapalli@gmail.com>
2013-07-12 06:25:13 +0000

    vijay

--- web.js
+++ web.js
@@ -1,13 +1,27 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var fileContents = 'junk';
+
+var out = fs.readFileSync('index.html','utf8', function(err, data){
+	if (err) {
+    	return console.log(err);
+  	}
+	fileContents = data;
+}
+
 app.get('/', function(request, response) {
-	var buf = 'vijay'
-  response.send(buf);
+	fs.readFile(
+  	response.send(fileContents);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
